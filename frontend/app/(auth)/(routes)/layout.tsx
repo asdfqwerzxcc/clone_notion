92097c8a2144bec5f84f9e7c84359fec103ebb3f
asdfqwerzxcc <wowls7990@gmail.com>
2024-12-16 21:42:56 +0900

    (Frontend) notion clone 1day Enviroment setup and folder setup

---
+++ frontend/app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,9 @@
+const RooLayout= ({children}: {children:React.ReactNode;}) =>{
+    return(
+        <div className="h-full bg-red-500 text-white">
+            {children}
+        </div>
+    )
+}
+
+export default RooLayout
